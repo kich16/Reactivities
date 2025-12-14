5c05b944264f8c8969a88148822f10296b048f65
Stevan Mitikj <stevan.mitic@outlook.com>
2025-12-14 15:44:50 +0100

    End of section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,10 +1,16 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseApiController : ControllerBase
+    public abstract class BaseApiController : ControllerBase
     {
+    private IMediator? _mediator;
+
+        protected IMediator Mediator => _mediator
+        ??= HttpContext.RequestServices.GetRequiredService<IMediator>()
+        ?? throw new InvalidOperationException("The mediator service is unavailable");
     }
 }
