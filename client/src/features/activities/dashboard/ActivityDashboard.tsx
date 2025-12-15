a46e687f7a841a718d83b69d564a2675b50581b7
Stevan Mitikj <stevan.mitic@outlook.com>
2025-12-15 19:15:45 +0100

    End of section 5

---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,52 @@
+import { Grid2 } from "@mui/material"
+import ActivityList from "./ActivityList"
+import ActivityDetails from "../details/ActivityDetails"
+import ActivityForm from "../form/ActivityForm"
+
+type Props = {
+    activities: Activity[]
+    selectedActivity?: Activity
+    selectActivity: (id: string) => void
+    cancelSelectActivity: () => void
+    editMode: boolean
+    closeForm: () => void
+    openForm: (id: string) => void
+    submitForm: (activity: Activity) => void
+    deleteActivity: (id: string) => void
+}
+
+export default function ActivityDashboard({
+    activities,
+    selectedActivity,
+    selectActivity,
+    cancelSelectActivity,
+    editMode,
+    closeForm,
+    openForm,
+    submitForm,
+    deleteActivity }: Props) {
+    return (
+        <Grid2 container spacing={3}>
+            <Grid2 size={7}>
+                <ActivityList
+                    activities={activities}
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity} />
+            </Grid2>
+            <Grid2 size={5}>
+                {selectedActivity && !editMode &&
+                    <ActivityDetails
+                        activity={selectedActivity}
+                        cancelSelectActivity={cancelSelectActivity}
+                        openForm={openForm}
+                        />}
+
+                {editMode &&
+                    <ActivityForm
+                        closeForm={closeForm}
+                        activity={selectedActivity}
+                        submitForm={submitForm} />}
+            </Grid2>
+        </Grid2>
+    )
+}
